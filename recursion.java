d2129f5ecc2f23c125b008afcb3b4f2ce7f3a654
Amanda Chen <achen01@stuy.edu>
2019-01-31 12:14:52 -0500

    added sqrt method

--- recursion.java
+++ recursion.java
@@ -1,3 +1,26 @@
 public class recursion{
-  
+  public static double sqrt(double n){
+    if (n == 0){
+      return 0;
+    }
+    return rootHelp(n, 1);
+  }
+
+  public static double rootHelp(double n, double guess){
+    if (Math.abs(guess*guess - n) < n * 0.00001){
+      return guess;
+    } else {
+      guess = (n /guess + guess) / 2;
+      return rootHelp(n, guess);
+    }
+  }
+
+  public static void main(String[] args){
+    System.out.println(sqrt(4));
+    System.out.println(sqrt(0.000000003));
+    System.out.println(sqrt(4808788));
+    System.out.println(sqrt(24));
+    System.out.println(sqrt(12345.64));
+    System.out.println(sqrt(0));
+  }
 }
